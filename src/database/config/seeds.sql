089d5d6a120f4ef113e7226f050cb897b85aa52a
SaifHayek <seefhayek4@gmail.com>
2022-09-26 10:47:34 +0300

    make schema with seeder sql close #6

---
+++ src/database/config/seeds.sql
@@ -0,0 +1,42 @@
+BEGIN;
+
+INSERT INTO CLIENT(USERNAME, PASSWORD)
+ VALUES ('FADEZAK100', '$2a$12$JfAC2rit./pFO4uOkwZAGOFjH8OVQozOyG9ecNT9yoBk8VpvtzQoS'),
+  ('saif', '$2a$12$JfAC2rit./pFO4uOkwZAGOFjH8OVQozOyG9ecNT9yoBk8VpvtzQoS'),
+  ('salsabeel', '$2a$12$JfAC2rit./pFO4uOkwZAGOFjH8OVQozOyG9ecNT9yoBk8VpvtzQoS'),
+  ('nader', '$2a$12$JfAC2rit./pFO4uOkwZAGOFjH8OVQozOyG9ecNT9yoBk8VpvtzQoS');
+
+
+
+INSERT INTO PRODUCTS(NAME, DESCRIPTION, PRICE, STOCK, CATEGORY, IMAGE, GALARY) VALUES 
+('IPHONE 9', 'THIS IS IPHONE 9', 99.99, 10, 'SMART PHONES', 'https://dummyjson.com/image/i/products/1/thumbnail.jpg',
+ARRAY[
+'https://dummyjson.com/image/i/products/1/1.jpg',
+'https://dummyjson.com/image/i/products/1/2.jpg']
+),
+('Samsung', 'THIS IS smasung', 150, 11, 'smart watch', 'https://dummyjson.com/image/i/products/1/thumbnail.jpg',
+ARRAY[
+'https://dummyjson.com/image/i/products/1/1.jpg',
+'https://dummyjson.com/image/i/products/1/2.jpg']
+),
+('Xiamoi', 'THIS IS xiaomi', 20, 9, 'laptops', 'https://dummyjson.com/image/i/products/1/thumbnail.jpg',
+ARRAY[
+'https://dummyjson.com/image/i/products/1/1.jpg',
+'https://dummyjson.com/image/i/products/1/2.jpg']
+); 
+
+
+
+INSERT INTO CARTS(PRODUCT_ID, CLIENT_ID, QUANTITY) VALUES
+(1, 1, 10),
+(2, 1, 10),
+(1, 2, 5),
+(3, 2, 10);
+
+
+
+
+
+
+
+COMMIT;
