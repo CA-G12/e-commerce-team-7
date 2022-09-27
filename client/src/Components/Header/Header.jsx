0f3d887694a9b090e56daebc7ecc35dae606345f
SaifHayek <seefhayek4@gmail.com>
2022-09-27 14:42:35 +0300

    making header style with css close #26

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,10 +1,33 @@
 import './style.css';
+import { Link, NavLink } from 'react-router-dom';
 
 function Header() {
+
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <div className="navbar">
+        <h1 className="header-title">E-commerce</h1>
+        <nav>
+          <NavLink  to="/" end className={({isActive}) => isActive ? 'active':'in-active'}>
+            Home
+          </NavLink>
+          <NavLink  to="/products" className={({isActive}) => isActive ? 'active':'in-active'}>
+            Products
+          </NavLink>
+          <NavLink  to="/cart" className={({isActive}) => isActive ? 'active':'in-active'}>
+            Cart
+          </NavLink>
+        </nav>
+        <div className="auth-buttons">
+          <button type="button" className="btn-class">
+            <Link to="/signUp">Sign Up</Link>
+          </button>
+          <button type="button" className="btn-class">
+            <Link to="/signIn">Sign In</Link>
+          </button>
+        </div>
+      </div>
+    </header>
   );
 }
 
