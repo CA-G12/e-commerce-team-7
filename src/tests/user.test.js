48f68df40de76e405518d8cb3df316a2282ba59d
Nader-SH <nadershak149@gmail.com>
2022-09-27 14:01:57 +0300

    create landing card and tests

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -66,3 +66,65 @@ describe('signup router', () => {
       });
   });
 });
+
+describe('login router', () => {
+  test('test login query exist data', (done) => {
+    request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        username: 'nasssssssssss',
+        password: '123456',
+      })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.username).toBe('nasssssssssss');
+        done();
+      });
+  });
+  test('test login query user not found', (done) => {
+    request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        username: 'ahmad',
+        password: '123456',
+      })
+      .expect(400)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body).toBe('Username not found');
+        done();
+      });
+  });
+  test('test login query wrong password', (done) => {
+    request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        username: 'nasssssssssss',
+        password: '1234567',
+      })
+      .expect(400)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body).toBe('wrong password');
+        done();
+      });
+  });
+});
+
+describe('logout router', () => {
+  test('test logout query', (done) => {
+    request(app)
+      .post('/api/v1/auth/logout')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body).toBe('logged out successfully');
+        done();
+      });
+  });
+});
