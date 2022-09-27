ddd6c6b9771dcdceb983e13f41b038c42b2ef694
SaifHayek <seefhayek4@gmail.com>
2022-09-27 22:26:58 +0300

    fetch data from database related $42

--- client/src/Components/ProductsPage/ProductsPage.jsx
+++ client/src/Components/ProductsPage/ProductsPage.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './style.css';
+import Card from '../ProductCard/Card';
 
 function ProductsPage() {
-  return <div>ProductsPage</div>;
+  const [Products, setProducts] = useState([]);
+  useEffect(() => {
+    axios
+      .get('/api/v1/product')
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div className="product-container">
+      {Products.map((ele) => (
+        <Card product={ele} />
+      ))}
+    </div>
+  );
 }
 
 export default ProductsPage;
