d1d0d2e6e692f2a66a08e493f3bd8b636c54355f
salsabeelomar <omarsalsabeel65@gmail.com>
2022-09-28 23:56:41 +0300

    resolve Conflict

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
 import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import axios from 'axios';
 import fetchCartItem from './Components/Cart/fetch';
+
 import {
   Header,
   LandingCard,
@@ -11,30 +15,54 @@ import {
   Login,
 } from './Components';
 
-import './App.css';
-
 function App() {
   const [cart, setCart] = useState([
     {
       data: 'Nodata',
     },
   ]);
+  const [isLogged, setLogged] = useState(false);
+  const [username, setUsername] = useState('');
+
   useEffect(() => {
     fetchCartItem().then((data) => setCart(data));
+  }, []);
+
+  useEffect(() => {
+    axios('/api/v1/auth/verify')
+      .then((res) => {
+        if (res.status === 200) {
+          setLogged(true);
+          setUsername(res.data.username);
+        }
+      })
+      .catch(() => toast.info('You can login'));
   }, []);
   return (
     <div className="App">
       <Router>
-        <Header />
+        <Header isLogged={isLogged} setLogged={setLogged} username={username} />
         <Routes>
           <Route path="/cart" element={<Cart cart={cart} />} />
           <Route path="/products" element={<ProductsPage />} />
           <Route end path="/" element={<LandingCard />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup setLogged={setLogged} />} />
+          <Route path="/login" element={<Login setLogged={setLogged} />} />
         </Routes>
       </Router>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
+
 export default App;
