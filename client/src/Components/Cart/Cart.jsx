57e141f0fabac5254072df0c05fc2e95d6f12893
salsabeelomar <omarsalsabeel65@gmail.com>
2022-09-28 13:53:22 +0300

    create function for add cart

--- client/src/Components/Cart/Cart.jsx
+++ client/src/Components/Cart/Cart.jsx
@@ -1,7 +1,19 @@
+import React from "react";
+import ProductCart from './ProductCart';
+import './style.css';
+
 export default function Cart() {
-  return <div>
-    <div>
-        cart
+
+const [total,setTotal]=React.useState(4);
+
+  return (
+    <div className="Cart-Container">
+      <div className="Header">
+        <h3 className="Heading">Shopping Cart</h3>  
+        <button type='button'> Total price $ {total} </button>
+      </div>
+      <ProductCart total = {total} setTotal = {setTotal} />
+
     </div>
-  </div>;
+  );
 }
--- client/src/Components/Cart/ProductCart.jsx
+++ client/src/Components/Cart/ProductCart.jsx
@@ -1,12 +1,45 @@
-export default function ProductCart(){
-    return <div>
-        <img alt='product' src="https://dummyjson.com/image/i/products/2/3.jpg" />
-        <div>
-              <p> name </p>
-              <input type='number'/>
-              <p> $444 </p>
-        </div>
-      
-    </div>
+import React from "react";
+import PropTypes from 'prop-types';
+
+export default function ProductCart({ setTotal, total }) {
+    const [nmbProduct, setNmbProduct ] = React.useState(1);
     
-} 
+  return (
+    <div className="Cart-Items">
+      <div className="image-box">
+        <img
+          src="https://dummyjson.com/image/i/products/2/3.jpg"
+          alt="product "
+        />
+      </div>
+      <div className="about">
+        <h1 className="title">name</h1>
+        <h3 className="subtitle">$41</h3>
+      </div>
+      <div className="counter">
+        <button type="button" className="btn" onClick={()=>{
+            setNmbProduct((prev)=>prev+1);
+            const price = total + 4;
+            setTotal(price);
+        }} >
+          +
+        </button>
+        <div className="count">{nmbProduct}</div>
+        <button type="button" className="btn" onClick={()=>{
+            if(nmbProduct > 1){
+            setNmbProduct(nmbProduct  - 1)
+            const price = total - 4;
+            setTotal(price);
+            }
+            
+        }}>
+          -
+        </button>
+      </div>
+    </div>
+  );
+}
+ProductCart.propTypes={
+    setTotal: PropTypes.func.isRequired,
+    total: PropTypes.number.isRequired
+}
